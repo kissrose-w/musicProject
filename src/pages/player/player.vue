c6d12b059c78d56ca320eeef500c28a77c09e64f
tl <3323998925@qq.com>
2025-12-03 22:00:23 +0800

    上传排行榜

---
+++ src/pages/player/player.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"> 
+
+</script>
+
+<template>
+<view>gedan</view>
+
+</template>
+
+<style lang="scss" scoped>
+
+
+</style>
---
+++ src/services/type.d.ts
@@ -0,0 +1,4 @@
+export type officialChart = {
+  code:number,
+  list:[]
+}
