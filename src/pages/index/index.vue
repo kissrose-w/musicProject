d2a784a085ab07b864833a44f21f3e3b26975a07
zly <2849688106@qq.com>
2025-12-04 17:56:57 +0800

    修改

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <view class="content">
     <Head></Head>
     <swiper class="swipers" 
@@ -76,26 +75,4 @@ homePage()
   }
 	
 	
-=======
-  <view @click="goSearch" class="content">
-    搜索
-  </view>
-</template>
-
-<script setup lang="ts">
-import { useRouter } from 'vue-router'
-const router = useRouter()
-const goSearch = () => {
-  router.push({
-    path: 'pages/search/search'
-  })
-}
-</script>
-
-<style>
-.content{
-  border: 1px solid red;
-  font-size: 20px;
-}
->>>>>>> mlh
 </style>
