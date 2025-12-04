3c3420fffe0cda86bde97c2905f552ed45622cbf
mlh <1623369197@qq.com>
2025-12-04 10:19:14 +0800

    Revert "搜索"
    This reverts commit a3e39b63df33b2931fb7c21558471f650e1fce7f.

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,8 +1,23 @@
 <template>
+  <view @click="goSearch" class="content">
+    搜索
+  </view>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const goSearch = () => {
+  router.push({
+    path: 'pages/search/search'
+  })
+}
 </script>
 
 <style>
+.content{
+  border: 1px solid red;
+  font-size: 20px;
+}
 </style>
