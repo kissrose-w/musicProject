5da45acfa573923a902074f682f8d61b0f3112de
mlh <1623369197@qq.com>
2025-12-05 22:47:20 +0800

    我的按钮

--- src/pages/mine/mine.vue
+++ src/pages/mine/mine.vue
@@ -1,45 +1,58 @@
 <script lang="ts" setup>
 import { mineApi } from "../../services/index"
 import { ref } from "vue"
-const mineInfo = ref({})
+
+interface MineInfo {
+  id: number;
+  coverImgUrl: string;
+  name: string;
+  playCount: number;
+  subscribedCount: number;
+}
+
+const show = ref<boolean>(false)
+const mineInfo = ref<MineInfo>()
 const getMine = async() => {
   try {
     const res = await mineApi()
     console.log(res.playlist[0])
     mineInfo.value = res.playlist[0]
+    console.log(mineInfo.value)
   } catch (error) {
     console.log(error)
   }
 }
 getMine()
 const toMyLike = (e:any) => {
-  const { id } = e.target.dataset
+  const { id } = e.currentTarget.dataset
   uni.navigateTo({
-    url:`/pages/player/player?id=${id}`
+    url:`/pages/playList/playList?id=${id}`
   }) 
 }
 </script>
 
 <template>
-  <view class="basicInfo">
-    <view class="avatar">
-      <img src="https://p1.music.126.net/q7pUyLVm3g6uL2QQeMvSgg==/109951168645624240.jpg" alt="">
+  <view v-if="show" class="con">
+    <view class="basicInfo">
+      <view class="avatar">
+        <img src="https://p1.music.126.net/q7pUyLVm3g6uL2QQeMvSgg==/109951168645624240.jpg" alt="">
+      </view>
+      <view class="infoNav">
+        <view>关注</view>
+        <view>粉丝</view>
+        <view>等级</view>
+        <view>听歌</view>
+      </view>
     </view>
-    <view class="infoNav">
-      <view>关注</view>
-      <view>粉丝</view>
-      <view>等级</view>
-      <view>听歌</view>
+    <view class="myLike" :data-id="mineInfo?.id" @click="toMyLike">
+      <img :src="mineInfo?.coverImgUrl" alt="">
+      <view class="likeInfo">
+        <text>{{ mineInfo?.name }}</text>
+        <view class="count">{{mineInfo?.playCount}}首·{{mineInfo?.subscribedCount}}次播放</view>
+      </view>
     </view>
   </view>
-  <view class="myLike" :data-id="mineInfo.id" @click="toMyLike">
-    <img :src="mineInfo.coverImgUrl" alt="">
-    <view class="likeInfo">
-      <text>{{ mineInfo.name }}</text>
-      <view class="count">{{mineInfo.playCount}}首·{{mineInfo.subscribedCount}}次播放</view>
-    </view>
-  </view>
-  <!-- <button @click="gode">去登陆</button> -->
+  <button v-if="!show" @click="show = true">去登陆</button>
 </template>
 
 <style lang="scss" scoped>
