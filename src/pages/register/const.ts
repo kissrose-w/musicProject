84604134675c917f64710a6d0c9d591964f2ed11
mlh <1623369197@qq.com>
2025-12-06 13:52:36 +0800

    注册

--- src/pages/register/const.ts
+++ src/pages/register/const.ts
@@ -1,5 +1,13 @@
 import { reactive } from "vue"
 
+export type data = {
+  name: string,
+  pas: string,
+  isPas: string,
+  tel: number | null,
+  cap: number | null,
+}
+
 export const rules = reactive({
 	// 对name字段进行必填验证
 	name: {
@@ -14,7 +22,7 @@ export const rules = reactive({
 			{
 				minLength: 3,
 				maxLength: 5,
-				errorMessage: '{label}长度在 {minLength} 到 {maxLength} 个字符',
+				errorMessage: '请输入 {minLength} 到 {maxLength} 位昵称',
 			}
 		],
 		// 当前表单域的字段中文名，可不填写
@@ -30,7 +38,7 @@ export const rules = reactive({
 			{
 				minLength: 3,
 				maxLength: 5,
-				errorMessage: '{label}长度在 {minLength} 到 {maxLength} 个字符',
+				errorMessage: '请输入 {minLength} 到 {maxLength} 位密码',
 			}
 		],
 		label:'密码',
@@ -42,10 +50,14 @@ export const rules = reactive({
 				required: true,
 				errorMessage: '请确认密码',
 			},
+      {
+        validator: (value: string, form: {pas: string, isPas: string}) => value === form.pas, 
+        errorMessage: '两次输入的密码不一致' 
+      },
 			{
 				minLength: 3,
 				maxLength: 5,
-				errorMessage: '{label}长度在 {minLength} 到 {maxLength} 个字符',
+				errorMessage: '请输入 {minLength} 到 {maxLength} 位密码',
 			}
 		],
 		label:'确认密码',
@@ -56,17 +68,34 @@ export const rules = reactive({
 			{
 				required: true,
 				errorMessage: '请输入手机号',
-        validateFunction() {
-          
-        }
 			},
+      {
+        pattern: /^1[3-9]\d{9}$/, // 手机号正则校验
+        errorMessage: '请输入正确的手机号格式',
+      },
 			{
 				minLength: 11,
 				maxLength: 11,
-				errorMessage: '{label}长度为 {minLength} 个字符',
+				errorMessage: '请输入 {minLength} 位数字',
 			}
 		],
 		label:'手机号',
 		validateTrigger:'submit'
 	},
+	cap: {
+		rules:[
+			{
+				required: true,
+				errorMessage: '请输入验证码',
+			},
+			{
+				minLength: 4,
+				maxLength: 4,
+				errorMessage: '请输入 {minLength} 位数字',
+			}
+		],
+		label:'验证码',
+		validateTrigger:'submit'
+	},
 })
+
--- src/pages/register/register.vue
+++ src/pages/register/register.vue
@@ -1,14 +1,7 @@
 
 <script setup lang='ts'>
-import { reactive } from 'vue'
-
-interface data {
-  name: string,
-  pas: string,
-  isPas: string,
-  tel: number | null,
-  cap: number | null,
-}
+import { reactive, ref } from 'vue'
+import { rules, type data } from './const'
 
 const formData = reactive<data>({
   name: '',
@@ -17,6 +10,19 @@ const formData = reactive<data>({
   tel: null,
   cap: null
 })
+const form = ref(null)
+
+const submit = () => {
+	// 优先校验两次密码是否一致
+	if (formData.pas !== formData.isPas) {
+		// 使用 uni.showToast 提示用户（uni-app 全局API）
+		// icon: 'none' 显示普通文本提示
+		// 若你的环境没有 uni，全局提示可以替换为其他实现
+		uni.showToast({ title: '两次输入的密码不一致', icon: 'none' })
+		return
+	}
+	form.value.validate()
+}
 
 
 
@@ -25,16 +31,16 @@ const formData = reactive<data>({
 </script>
 
 <template>
-  <view class="title">注册</view>
-  <view class="">
-		<uni-forms :modelValue="formData">
+  <view class="page">
+    <view class="title">注册</view>
+		<uni-forms ref="form" :modelValue="formData" :rules="rules">
 			<uni-forms-item required label="昵称" name="name">
 				<uni-easyinput type="text" v-model="formData.name" placeholder="请输入昵称" />
 			</uni-forms-item>
 			<uni-forms-item required label="密码" name="pas">
 				<uni-easyinput type="text" v-model="formData.pas" placeholder="请输入密码" />
 			</uni-forms-item>
-			<uni-forms-item required name="isPas" label="确认密码">
+			<uni-forms-item required name="isPas" label="确认密码" width="60px">
 				<uni-easyinput type="text" v-model="formData.isPas" placeholder="确认密码" />
 			</uni-forms-item>
 			<uni-forms-item required name="tel" label="手机号">
@@ -44,7 +50,7 @@ const formData = reactive<data>({
 				<uni-easyinput type="number" v-model="formData.cap" placeholder="请输入验证码" />
 			</uni-forms-item>
 		</uni-forms>
-		<button>提交</button>
+		<button @click="submit">提交</button>
 	</view>
 </template>
 
@@ -52,5 +58,8 @@ const formData = reactive<data>({
   .title{
     font-size: 50px;
     font-weight: 700;
+  }
+  .page{
+    padding: 10px;
   }
 </style>
